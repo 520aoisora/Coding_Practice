0bacb9396a272c85bcf10ca0ea64e41f86db15af
520aoisora <520aoisora@gmail.com>
2013-10-24 22:29:30 +0100

    add chapter1 question5

---
+++ Coding_Practice/Chapter1Question5.h
@@ -0,0 +1,15 @@
+#ifndef _CHAPTER1_QUESTION5_H_
+#define _CHAPTER1_QUESTION5_H_
+
+#include <string>
+
+namespace C1Q5
+{
+	void append(std::string& newStr, char lastChar, int count);
+
+	bool compressString(std::string& str);
+
+	void run();
+}
+
+#endif
--- Coding_Practice/main.cpp
+++ Coding_Practice/main.cpp
@@ -4,10 +4,11 @@
 #include "Chapter1Question2.h"
 #include "Chapter1Question3.h"
 #include "Chapter1Question4.h"
+#include "Chapter1Question5.h"
 
 int main()
 {
-	C1Q4::run();
+	C1Q5::run();
 
 	return 0;
 }
