df8c04b627779aaaf044c46abf40aaed7adbe02c
520aoisora <520aoisora@gmail.com>
2013-11-05 23:33:54 +0000

    edit chapter1 question2 to make it work in new frame :)

--- Coding_Practice/src/Chapter1/chapter1_question2.cpp
+++ Coding_Practice/src/Chapter1/chapter1_question2.cpp
@@ -1,3 +1,12 @@
+////////////////////////////////////////////////////////////////////////////////////////////////////
+//	Question description: Implement a function void reverse(char* str) in C++ which reverses a
+//						  null-terminated string.
+//
+//		File description: This file declares class C1Q2 with member function reverse(char *str)
+//						  to reverse a null-terminated string.
+//
+////////////////////////////////////////////////////////////////////////////////////////////////////
+
 // 1. Preferred location
 #include "chapter1_question2.h"
 
@@ -11,6 +20,28 @@
 // 5. Your project's .h files
 
 using namespace std;
+
+QuestionName C1Q2::question_name_ = "C1Q2";
+
+C1Q2 C1Q2::question_instance_(C1Q2::question_name_);
+
+ClassTemplate *C1Q2::CreateSpecificQuestionPointer()
+{
+	return new C1Q2();
+}
+
+void C1Q2::RunRegression()
+{
+	char str[] = "hello";
+
+	char *str1 = str;
+
+	reverse(str1);
+
+	cout << str1 << endl;
+
+	return;
+}
 
 void C1Q2::swap(char *begin, char *end)
 {
@@ -32,15 +63,3 @@ void C1Q2::reverse(char *str)
 		swap(begin, end);
 }
 
-void C1Q2::runRegression()
-{
-	char str[] = "hello";
-
-	char *str1 = str;
-
-	reverse(str1);
-
-	cout << str1 << endl;
-
-	return;
-}
--- Coding_Practice/src/Chapter1/chapter1_question2.h
+++ Coding_Practice/src/Chapter1/chapter1_question2.h
@@ -1,3 +1,12 @@
+////////////////////////////////////////////////////////////////////////////////////////////////////
+//	Question description: Implement a function void reverse(char* str) in C++ which reverses a
+//						  null-terminated string.
+//
+//		File description: This file declares class C1Q2 with member function reverse(char *str)
+//						  to reverse a null-terminated string.
+//
+////////////////////////////////////////////////////////////////////////////////////////////////////
+
 #ifndef CODING_PRACTICE_CHAPTER1_QUESTION2_H_
 #define CODING_PRACTICE_CHAPTER1_QUESTION2_H_
 
@@ -17,7 +26,21 @@ class C1Q2 : public ClassTemplate
 
 public:
 
-	void runRegression();
+	C1Q2() : ClassTemplate() {};
+
+	C1Q2(QuestionName iQuestionName) : ClassTemplate(iQuestionName) {};
+
+	void RunRegression();
+
+protected:
+
+	ClassTemplate *CreateSpecificQuestionPointer();
+
+private:
+
+	static QuestionName question_name_;
+
+	static C1Q2 question_instance_;
 
 private:
 
