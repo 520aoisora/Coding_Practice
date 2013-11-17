c493f4c089af815b9cbed638e1579952c6561fc7
520aoisora <520aoisora@gmail.com>
2013-11-17 17:43:09 +0000

    refactor C1Q6
    rewrite C1Q6 as the old implementation was wrong

--- Coding_Practice/src/Chapter1/chapter1_question6.cpp
+++ Coding_Practice/src/Chapter1/chapter1_question6.cpp
@@ -1,3 +1,12 @@
+////////////////////////////////////////////////////////////////////////////////////////////////////
+//	Question description: Given an image represented by an NxN matrix, where each pixel in the image
+//						  is 4 bytes, write a method to rotate the image by 90 degrees. Do it in place.
+//
+//		File description: This file declares class C1Q6 with member function rotate5x5Matrix to rotate
+//						  a 5 x 5 integer matrix by 90 degrees in place.
+//
+////////////////////////////////////////////////////////////////////////////////////////////////////
+
 // 1. Preferred location
 #include "chapter1_question6.h"
 
@@ -12,43 +21,58 @@
 
 using namespace std;
 
-bool C1Q6::areEqualArray2(int matrix1[2][2], int matrix2[2][2])
+QuestionName C1Q6::question_name_ = "C1Q6";
+
+C1Q6 C1Q6::question_instance_(C1Q6::question_name_);
+
+ClassTemplate *C1Q6::CreateSpecificQuestionPointer()
 {
-	for(int i = 0; i < 2; i++)
-		for(int j = 0; j < 2; j++)
-			if(matrix1[i][j] != matrix2[i][j])
+	return new C1Q6();
+}
+
+bool C1Q6::areTwo5x5ArrayEqual(int matrix1[5][5], int matrix2[5][5])
+{
+	for (int row = 0; row < 4; row++)
+		for (int column = 0; column < 4; column++)
+			if (matrix1[row][column] != matrix2[row][column])
 				return false;
 
 	return true;
 }
 
-void C1Q6::rotate2(int matrix[2][2], int N)
+void C1Q6::rotate5x5Matrix(int ioMatrix[5][5], int N)
 {
-	for(int row = 0; row < (N / 2); row++)
+	for (int row = 0; row <= N / 2 - 1; row++)
 	{
-		for(int column = 0; column < ((N+1) / 2); column++)
+		for (int column = 0; column <= (N + 1) / 2 - 1; column++)
 		{
-			int temp = matrix[row][column];
-			matrix[row][column] = matrix[row][N-1-column];
-			matrix[row][N-1-column] = matrix[N-1-row][N-1-column];
-			matrix[N-1-row][N-1-column] = matrix[N-1-row][column];
-			matrix[N-1-row][column] = temp;
+			int buffer = ioMatrix[row][column];
+			ioMatrix[row][column] = ioMatrix[column][N - 1 - row];
+			ioMatrix[column][N - 1 - row] = ioMatrix[N - 1 - row][N - 1 - column];
+			ioMatrix[N - 1 - row][N - 1 - column] = ioMatrix[N - 1 - column][row];
+			ioMatrix[N - 1 - column][row] = buffer;
 		}
 	}
 }
 
-void C1Q6::runRegression()
+void C1Q6::RunRegression()
 {
-	int matrix1[2][2] = {{1,2},
-							{3,4}};
+	int original_5x5_martix[5][5] = {{ 1, 2, 3, 4, 5 },
+									{ 6, 7, 8, 9, 10 },
+									{ 11, 12, 13, 14, 15 },
+									{ 16, 17, 18, 19, 20 },
+									{ 21, 22, 23, 24, 25 }};
 
-	int resultMatrix1[2][2] = {{2,4},
-								{1,3}};
+	int rotated_5x5_martix[5][5] = {{ 5, 10, 15, 20, 25 },
+									{ 4, 9, 14, 19, 24 },
+									{ 3, 8, 13, 18, 23 },
+									{ 2, 7, 12, 17, 22 },
+									{ 1, 6, 11, 16, 21 } };
 
-	rotate2(matrix1,2);
+	rotate5x5Matrix(original_5x5_martix, 5);
 
 	cout << "Test 1: ";
-	if(areEqualArray2(matrix1, resultMatrix1))
+	if (areTwo5x5ArrayEqual(original_5x5_martix, rotated_5x5_martix))
 		cout << "OK";
 	else
 		cout << "Fail";
--- Coding_Practice/src/Chapter1/chapter1_question6.h
+++ Coding_Practice/src/Chapter1/chapter1_question6.h
@@ -1,3 +1,12 @@
+////////////////////////////////////////////////////////////////////////////////////////////////////
+//	Question description: Given an image represented by an NxN matrix, where each pixel in the image
+//						  is 4 bytes, write a method to rotate the image by 90 degrees. Do it in place.
+//
+//		File description: This file declares class C1Q6 with member function rotate5x5Matrix to rotate
+//						  a 5 x 5 integer matrix by 90 degrees in place.
+//
+////////////////////////////////////////////////////////////////////////////////////////////////////
+
 #ifndef CODING_PRACTICE_CHAPTER1_QUESTION6_H_
 #define CODING_PRACTICE_CHAPTER1_QUESTION6_H_
 
@@ -17,13 +26,52 @@ class C1Q6 : public ClassTemplate
 
 public:
 
-	void runRegression();
+	C1Q6() : ClassTemplate() {};
+
+	C1Q6(QuestionName iQuestionName) : ClassTemplate(iQuestionName) {};
+
+	void RunRegression();
+
+protected:
+
+	ClassTemplate *CreateSpecificQuestionPointer();
 
 private:
 
-	bool areEqualArray2(int matrix1[2][2], int matrix2[2][2]);
+	static QuestionName question_name_;
 
-	void rotate2(int matrix[2][2], int N);
+	static C1Q6 question_instance_;
+
+private:
+
+	bool areTwo5x5ArrayEqual(int matrix1[5][5], int matrix2[5][5]);
+
+	/*
+		@ Description:
+		Rotate an integer 5x5 matrix by 90 degrees.
+
+		@ Algorithm:
+		By using a temporary value to rotate the whole matrix in place.
+
+		Time efficiency: O(NxN)
+		Space efficiency: O(1)
+
+		@ Input:
+		ioMatrix: Input integer matrix, size 5 x 5.
+			   N: Size of the matrix. N = 5 all the time in this case.
+
+		@ Output:
+		ioMatrix: A 5 x 5 integer matrix rotated by 90 degrees.
+
+		@ Usage:
+		int original_5x5_martix[5][5] = {{ 1, 2, 3, 4, 5 },
+										{ 6, 7, 8, 9, 10 },
+										{ 11, 12, 13, 14, 15 },
+										{ 16, 17, 18, 19, 20 },
+										{ 21, 22, 23, 24, 25 }};
+		rotate5x5Matrix(original_5x5_martix, 5);
+	*/
+	void rotate5x5Matrix(int ioMatrix[5][5], int N);
 };
 
 #endif
